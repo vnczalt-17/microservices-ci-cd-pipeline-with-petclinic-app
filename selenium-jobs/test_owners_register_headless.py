8fbf03636ea8950369fb30803c02fa81a586940d
Vincenzo <vncz.17@gmail.com>
2020-10-24 19:58:13 +0300

    updated selenium job

--- selenium-jobs/test_owners_register_headless.py
+++ selenium-jobs/test_owners_register_headless.py
@@ -25,16 +25,22 @@ sleep(2)
 fn_field = driver.find_element_by_name('firstName')
 fn = 'Callahan' + str(random.randint(0, 100))
 fn_field.send_keys(fn)
+sleep(1)
 fn_field = driver.find_element_by_name('lastName')
 fn_field.send_keys('Clarusway')
+sleep(1)
 fn_field = driver.find_element_by_name('address')
 fn_field.send_keys('Ridge Corp. Street')
+sleep(1)
 fn_field = driver.find_element_by_name('city')
 fn_field.send_keys('McLean')
+sleep(1)
 fn_field = driver.find_element_by_name('telephone')
 fn_field.send_keys('+1230576803')
+sleep(1)
 fn_field.send_keys(Keys.ENTER)
-# Wait 1 second to get updated Owner List
+sleep(1)
+# Wait 2 second to get updated Owner List
 sleep(2)
 # Verify that new user is added to Owner List
 if fn in driver.page_source:
